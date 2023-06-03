76bc1ce8d8b8bf48b38191e5c3b934f2a8fa7081
JoaoFelipeS <joaofelipe>
2023-06-03 19:27:08 -0300

    Todas as telas e menu funcionando, listview corrigido

--- app/src/main/java/com/cinereview/ListaFilmesActivity.java
+++ app/src/main/java/com/cinereview/ListaFilmesActivity.java
@@ -2,10 +2,16 @@ package com.cinereview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ListView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ListaFilmesActivity extends AppCompatActivity {
 
@@ -13,8 +19,48 @@ public class ListaFilmesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_filmes);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setBackgroundDrawable(null);
 
+        // Remove o título
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setBackgroundDrawable(null);
 
+        // Remover a barra de status
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            switch (itemId) {
+                case R.id.menu_item1:
+                    // Navegar para a tela de busca
+                    Intent intentBusca = new Intent(ListaFilmesActivity.this, BuscaActivity.class);
+                    startActivity(intentBusca);
+                    return true;
+                case R.id.menu_item2:
+                    // Navegar para a tela de recomendações
+                    Intent intentRecomendacoes = new Intent(ListaFilmesActivity.this, RecomendacoesActivity.class);
+                    startActivity(intentRecomendacoes);
+                    return true;
+                case R.id.menu_item3:
+                    // Navegar para a tela de home
+                    Intent intentHome = new Intent(ListaFilmesActivity.this, HomeActivity.class);
+                    startActivity(intentHome);
+                    return true;
+                case R.id.menu_item4:
+                    // Navegar para a tela de minhas celebridades
+                    Intent intentCelebridades = new Intent(ListaFilmesActivity.this, MinhasCelebridadesActivity.class);
+                    startActivity(intentCelebridades);
+                    return true;
+                case R.id.menu_item5:
+                    // Navegar para a tela de minha lista(esta tela)
+                    // Não é necessário fazer nada, já estamos na tela de home
+                    return true;
+                default:
+                    return false;
+            }
+        });
         ArrayList<Filme> listaFilmes = new ArrayList<>();
 
         Ator[] atores = {
--- app/src/main/java/com/cinereview/RecomendacoesActivity.java
+++ app/src/main/java/com/cinereview/RecomendacoesActivity.java
@@ -1,34 +1,21 @@
 package com.cinereview;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
-import android.widget.ListView;
-
-
-
-public class HomeActivity extends AppCompatActivity {
-
-    private ListView listViewMyList;
-    private ListView listViewRecomendation;
-    private ListView listViewCelebs;
-
-    private FilmeAdapter myListAdapter;
-    private FilmeAdapter recomendationAdapter;
-    private FilmeAdapter celebsAdapter;
+public class RecomendacoesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        setContentView(R.layout.activity_recomendacoes);
         // Remove o título
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
         getSupportActionBar().setBackgroundDrawable(null);
@@ -42,39 +29,32 @@ public class HomeActivity extends AppCompatActivity {
             switch (itemId) {
                 case R.id.menu_item1:
                     // Navegar para a tela de busca
-                    Intent intentBusca = new Intent(HomeActivity.this, BuscaActivity.class);
+                    Intent intentBusca = new Intent(RecomendacoesActivity.this, BuscaActivity.class);
                     startActivity(intentBusca);
                     return true;
                 case R.id.menu_item2:
-                    // Navegar para a tela de recomendações
-                    Intent intentRecomendacoes = new Intent(HomeActivity.this, RecomendacoesActivity.class);
-                    startActivity(intentRecomendacoes);
-                    return true;
-                case R.id.menu_item3:
-                    // Navegar para a tela de home (esta tela)
+                    // Navegar para a tela de recomendações(esta tela)
                     // Não é necessário fazer nada, já estamos na tela de home
                     return true;
+                case R.id.menu_item3:
+                    // Navegar para a tela de home
+                    Intent intentHome = new Intent(RecomendacoesActivity.this, HomeActivity.class);
+                    startActivity(intentHome);
+                    return true;
+
                 case R.id.menu_item4:
                     // Navegar para a tela de minhas celebridades
-                    Intent intentCelebridades = new Intent(HomeActivity.this, MinhasCelebridadesActivity.class);
+                    Intent intentCelebridades = new Intent(RecomendacoesActivity.this, MinhasCelebridadesActivity.class);
                     startActivity(intentCelebridades);
                     return true;
                 case R.id.menu_item5:
                     // Navegar para a tela de minha lista
-                    Intent intentMinhaLista = new Intent(HomeActivity.this, ListaFilmesActivity.class);
+                    Intent intentMinhaLista = new Intent(RecomendacoesActivity.this, ListaFilmesActivity.class);
                     startActivity(intentMinhaLista);
                     return true;
                 default:
                     return false;
             }
         });
-
-        listViewMyList = findViewById(R.id.listView_myList);
-        listViewRecomendation = findViewById(R.id.listView_Recomendation);
-        listViewCelebs = findViewById(R.id.listViewcelebs);
-
-
     }
-
-
 }
