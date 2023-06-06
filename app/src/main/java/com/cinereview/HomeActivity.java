dd16eaee57156ca10111eb098f2e9e1516d28aec
Jonathan Borges <mborges.jonathan@gmail.com>
2023-06-05 22:22:55 -0300

    home finalizado, usando favoritar e desfavoritar

--- app/src/main/java/com/cinereview/HomeActivity.java
+++ app/src/main/java/com/cinereview/HomeActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -27,29 +28,20 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 public class HomeActivity extends AppCompatActivity {
 
-    private ListView listViewMyList;
-    private ListView listViewRecomendation;
-    private ListView listViewCelebs;
+    private FirebaseAuth auth;
+    private Button logout;
+    private FirebaseUser user;
 
-    private FilmeAdapter myListAdapter;
-    private FilmeAdapter recomendationAdapter;
-    private FilmeAdapter celebsAdapter;
+    private DatabaseReference filmesRef;
+    private DatabaseReference userFavoritesRef;
 
-    FirebaseAuth auth;
-    Button logout;
-    FirebaseUser user;
+    private ArrayList<Filme> todosFilmes;
+    private ArrayList<Filme> filmesFavoritos;
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference filmesRef = database.getReference().child("filmes");
+    private Gson gson;
 
-    //String userId = user.getUid();
-
-    ArrayList<Filme> todosFilmes = new ArrayList<>();
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,110 +55,98 @@ public class HomeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        // Inicializa os componentes da UI
+        initializeUI();
+
+        // Verifica se o usuário está logado
+        if (user == null) {
+            navigateToMainActivity();
+        } else {
+            setupLogoutButton();
+            setupBottomNavigationView();
+            setupFilmesRef();
+            setupUserFavoritesRef();
+            loadFilmes();
+        }
+    }
+
+    private void initializeUI() {
         auth = FirebaseAuth.getInstance();
         logout = findViewById(R.id.btnLogout);
         user = auth.getCurrentUser();
+        todosFilmes = new ArrayList<>();
+        filmesFavoritos = new ArrayList<>();
+        gson = new GsonBuilder().create();
+    }
 
-        String userId = user.getUid();
-        if (user == null) {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
+    private void navigateToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
-        }
+    private void setupLogoutButton() {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-
+                navigateToMainActivity();
             }
         });
+    }
+
+    private void setupBottomNavigationView() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             switch (itemId) {
                 case R.id.menu_item1:
-                    // Navegar para a tela de busca
-                    Intent intentBusca = new Intent(HomeActivity.this, BuscaActivity.class);
-                    startActivity(intentBusca);
+                    navigateToActivity(BuscaActivity.class);
                     return true;
                 case R.id.menu_item2:
-                    // Navegar para a tela de recomendações
-                    Intent intentRecomendacoes = new Intent(HomeActivity.this,
-                            RecomendacoesActivity.class);
-                    startActivity(intentRecomendacoes);
+                    navigateToActivity(RecomendacoesActivity.class);
                     return true;
                 case R.id.menu_item3:
-                    // Navegar para a tela de home (esta tela)
                     // Não é necessário fazer nada, já estamos na tela de home
                     return true;
                 case R.id.menu_item4:
-                    // Navegar para a tela de minhas celebridades
-                    Intent intentCelebridades = new Intent(HomeActivity.this,
-                            MinhasCelebridadesActivity.class);
-                    startActivity(intentCelebridades);
+                    navigateToActivity(MinhasCelebridadesActivity.class);
                     return true;
                 case R.id.menu_item5:
-                    // Navegar para a tela de minha lista
-                    Intent intentMinhaLista = new Intent(HomeActivity.this,
-                            ListaFilmesActivity.class);
-                    startActivity(intentMinhaLista);
+                    navigateToActivity(ListaFilmesActivity.class);
                     return true;
                 default:
                     return false;
             }
         });
+    }
 
+    private void navigateToActivity(Class<?> activityClass) {
+        Intent intent = new Intent(HomeActivity.this, activityClass);
+        startActivity(intent);
+    }
 
-//        filmesRef.push().setValue(filmeJson);
+    private void setupFilmesRef() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        filmesRef = database.getReference().child("filmes");
+    }
 
+    private void setupUserFavoritesRef() {
+        String userId = user.getUid();
+        userFavoritesRef = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("users")
+                .child(userId)
+                .child("favoritos");
+    }
 
-        // Obter o ID do usuário logado
-
-
-//        filmesRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                //todosFilmes.clear();
-//
-//                for (DataSnapshot filmeSnapshot : dataSnapshot.getChildren()) {
-//                    String filmeJson = filmeSnapshot.getValue(String.class);
-//
-//                    Gson gson = new Gson();
-//                    Filme filme = gson.fromJson(filmeJson, Filme.class);
-//
-//                    todosFilmes.add(filme);
-//                    System.out.println(todosFilmes);
-//                }
-//                LinearLayout horizontalLayout = findViewById(R.id.layoutFilmes);
-//                FilmeAdapterListView filmeAdapter = new FilmeAdapterListView(HomeActivity.this,
-//                        todosFilmes);
-//                for (int i = 0; i < filmeAdapter.getCount(); i++) {
-//                    View itemView = filmeAdapter.getView(i, null, horizontalLayout);
-//                    horizontalLayout.addView(itemView);
-//                }
-//                System.out.println(todosFilmes);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                // Tratar erro, catch
-//            }
-//        });
+    private void loadFilmes() {
         filmesRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot filmeSnapshot : dataSnapshot.getChildren()) {
                     String filmeJson = filmeSnapshot.getValue(String.class);
-                    Gson gson = new Gson();
-
                     Filme filme = gson.fromJson(filmeJson, Filme.class);
-                    // System.out.println(filmeJson);
-                    //System.out.println(filme.toString());
                     todosFilmes.add(filme);
                 }
                 LinearLayout horizontalLayout = findViewById(R.id.layoutFilmes);
@@ -175,42 +155,35 @@ public class HomeActivity extends AppCompatActivity {
                 for (int i = 0; i < filmeAdapter.getCount(); i++) {
                     View itemView = filmeAdapter.getView(i, null, horizontalLayout);
                     Filme filme = todosFilmes.get(i);
-                    String filmeId = filme.getId();
-                    itemView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-//                            // Verifica se o filme está nos favoritos do usuário
-//                                DatabaseReference userFavoritesRef = FirebaseDatabase.getInstance()
-//                                        .getReference()
-//                                        .child("favoritos")
-//                                        .child(userId); // Substitua 'userId' pela ID do usuário atual
-//
-//                                userFavoritesRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//                                    @Override
-//                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                                        if (dataSnapshot.hasChild(filmeId)) {
-//                                            // O filme já está nos favoritos, então exclua-o
-//                                            userFavoritesRef.child(filmeId).removeValue();
-//                                            Toast.makeText(HomeActivity.this, "Filme retirado dos favoritos", Toast.LENGTH_SHORT).show();
-//                                        } else {
-//                                            // O filme não está nos favoritos, então adicione-o
-//                                            String filmeJson = gson.toJson(filme);
-//                                            userFavoritesRef.child(filmeId).setValue(filmeJson);
-//                                            Toast.makeText(HomeActivity.this, "Filme adicionado aos favoritos", Toast.LENGTH_SHORT).show();
-//                                        }
-//                                    }
-//
-//                                    @Override
-//                                    public void onCancelled(@NonNull DatabaseError databaseError) {
-//                                        // Tratar erro, se necessário
-//                                    }
-//                                });
-
-                        }
-                    });
+                    setupFilmeItemClickListener(itemView, filme);
                     horizontalLayout.addView(itemView);
                 }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Tratar erro, se necessário
+            }
+        });
+
+        // Dentro do método setupFilmeItemClickListener(), após adicionar ou remover o filme dos favoritos
+        userFavoritesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot filmeSnapshot : dataSnapshot.getChildren()) {
+                    String filmeJson = filmeSnapshot.getValue(String.class);
+                    Filme filme = gson.fromJson(filmeJson, Filme.class);
+                    filmesFavoritos.add(filme);
+                }
+
+                LinearLayout horizontalLayout = findViewById(R.id.layoutFavoritos);
+                FilmeAdapterListView filmeAdapter = new FilmeAdapterListView(HomeActivity.this, filmesFavoritos);
+                for (int i = 0; i < filmeAdapter.getCount(); i++) {
+                    View itemView = filmeAdapter.getView(i, null, horizontalLayout);
+                    Filme filme = filmesFavoritos.get(i);
+                    setupFilmeItemClickListener(itemView, filme);
+                    horizontalLayout.addView(itemView);
+                }
             }
 
             @Override
@@ -220,4 +193,67 @@ public class HomeActivity extends AppCompatActivity {
         });
 
     }
+
+    private void setupFilmeItemClickListener(View itemView, Filme filme) {
+        String filmeId = filme.getId();
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userFavoritesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(filmeId)) {
+                            // O filme já está nos favoritos, então exclua-o
+                            userFavoritesRef.child(filmeId).removeValue();
+                            Toast.makeText(HomeActivity.this, "Filme retirado dos favoritos",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            String filmeJson = gson.toJson(filme);
+                            userFavoritesRef.child(filmeId).setValue(filmeJson);
+                            Toast.makeText(HomeActivity.this, "Filme adicionado aos favoritos",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        updateFavoritosView();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Tratar erro, se necessário
+                    }
+                });
+            }
+        });
+
+    }
+
+        private void updateFavoritosView() {
+        userFavoritesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                filmesFavoritos.clear();
+                for (DataSnapshot filmeSnapshot : dataSnapshot.getChildren()) {
+                    String filmeJson = filmeSnapshot.getValue(String.class);
+                    Filme filme = gson.fromJson(filmeJson, Filme.class);
+                    filmesFavoritos.add(filme);
+                }
+
+                LinearLayout horizontalLayout = findViewById(R.id.layoutFavoritos);
+                horizontalLayout.removeAllViews(); // Limpa as visualizações existentes
+
+                FilmeAdapterListView filmeAdapter = new FilmeAdapterListView(HomeActivity.this, filmesFavoritos);
+                for (int i = 0; i < filmeAdapter.getCount(); i++) {
+                    View itemView = filmeAdapter.getView(i, null, horizontalLayout);
+                    Filme filme = filmesFavoritos.get(i);
+                    setupFilmeItemClickListener(itemView, filme);
+                    horizontalLayout.addView(itemView);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Tratar erro, se necessário
+            }
+        });
+    }
+
 }
